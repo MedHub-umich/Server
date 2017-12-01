3eae9e5b02f1f759e6d21620b5dbd6d603cdf0c3
siegelty <siegelty@umich.edu>
2017-11-30 21:16:46 -0500

    Added switcher in add_data_func

--- api/add_data.py
+++ api/add_data.py
@@ -3,6 +3,16 @@ import settings
 
 db = settings.db
 
+class PacketTypes:
+    HEART_RATE = 3
+    ECG = 2
+    BREATHING_RATE = 4
+    TEMPERATURE = 5
+    BLOOD_PRESSURE = 1
+
+class PacketPeriods:
+    ECG = 1
+
 add_data = Blueprint('add_data', __name__)
 
 # body it expect..
@@ -21,10 +31,43 @@ add_data = Blueprint('add_data', __name__)
 # }
 @add_data.route('', methods=['GET', 'POST'])
 def add_data_func():
-    db.Users.insert_one({
-            "test": True
-    }) 
+    packets = request.get_json()['packets']
+    for packet in packets:
+        if packet.type == PacketTypes.HEART_RATE:
+            return ingestHeartRate(packet)
+        elif packet.type == PacketTypes.ECG:
+            return ingestECG(packet)
+        elif packet.type == PacketTypes.BREATHING_RATE:
+            return ingestBreathingRate(packet)
+        elif packet.type == PacketTypes.TEMPERATURE:
+            return ingestTemperature(packet)
+        elif packet.type == PacketTypes.BLOOD_PRESSURE:
+            return ingestBloodPressure(packet)
+
+def ingestECG(packet):
+    pass
+
+def ingestHeartRate(packet):
+    pass
+
+def ingestBreathingRate(packet):
+    pass
+
+def ingestTemperature(packet):
+    pass
+
+def ingestBloodPressure(packet):
+    pass
+
+def respond_success():
     return jsonify(), 200
+
+def respond_failure(message):
+    message = {"message": message}
+    return message, 500
+
+
+
 
 #HELP
 # db.Users.insert({"_id": 2, 
