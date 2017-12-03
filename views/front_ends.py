669248ddd0c36bea63827056531d700975e62454
Kishore-B-Rao <kishbrao@umich.edu>
2017-12-02 19:57:22 -0500

    fdjkls

--- views/front_ends.py
+++ views/front_ends.py
@@ -6,7 +6,7 @@ front_ends = Blueprint('front_ends', __name__, template_folder='templates')
 def main_index():
     return render_template("index.html")
 
-@front_ends.route('/dashboard')
+@front_ends.route('/user')
 def dashboard():
     return render_template("dashboard.html")
 
