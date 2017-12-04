112ec8c1b009d4e1a4a810d5e18f2c6fedb4446c
Kishore-B-Rao <kishbrao@umich.edu>
2017-12-04 11:05:52 -0500

    fjdksl

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask
 import config
 import settings
-import views
 
 app = Flask(__name__, template_folder='templates')
 
 app.config.from_object(config.Config)
 
-# Serving up views since '03
-app.register_blueprint(views.front_ends)
 
 settings.connect_to_db(app.config['DB_URL'], app.config['DB_NAME'])
 
 # API endpoints
 import api
+import views
 app.register_blueprint(api.add_data, url_prefix="/api/v1.0/add_data")
 app.register_blueprint(api.sensor_data, url_prefix="/api/v1.0/sensor")
 app.register_blueprint(api.alert, url_prefix="/api/v1.0/alert")
+# Serving up views since '03
+app.register_blueprint(views.front_ends)
 
 if __name__ == '__main__':
     # listen on external IPs
--- views/front_ends.py
+++ views/front_ends.py
@@ -1,6 +1,8 @@
 from flask import *
+import settings
 
 front_ends = Blueprint('front_ends', __name__, template_folder='templates')
+db = settings.db
 
 @front_ends.route('/')
 def main_index():
@@ -12,4 +14,24 @@ def dashboard():
 
 @front_ends.route('/users')
 def users():
-    return render_template("users.html")
+	dbUsers = db.Users.find({})
+	users = []
+	for user in dbUsers:
+		userReturn = {
+			'id': user['_id'],
+			'name': user['info']['first_name'] + " " + user['info']['last_name'],
+			'lastAccess': findLastTimestamp(user),
+			'userLink': 'user?userID=' + str(user['_id'])
+		}
+		users.append(userReturn)
+	options = {
+		"users": users
+	}
+	print(options)
+	return render_template("users.html", **options)
+
+def findLastTimestamp(user):
+    if 'ecg' in user and user['ecg']['data']:
+        return user['ecg']['data'][0]['time']
+    else :
+        return "No Check in Found"
